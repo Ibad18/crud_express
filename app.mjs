888ab8e2a0040427efbad243383d9b59c0e1b9f7
Ibad18 <ibad1818@gmail.com>
2024-09-22 10:29:26 +0500

    Delete method created

--- app.mjs
+++ app.mjs
@@ -1,12 +1,15 @@
 import express from 'express'
 const app = express()
+// json middleware for parsing application/json
+app.use(express.json())
 const PORT = 5000
-const users = [
+let users = [
     {id: 1, name:"ibad", email:'ibad@gmail.com'},
     {id: 2, name:"ali", email:'ali@gmail.com'}
 ]
+
 app.get('/api/users', (req, res)=>{
-    res.status(200).json({message:'fetching users', data:users})
+    res.status(200).json({message:'fetching all users', data:users})
 })
 app.post('/api/users', (req, res)=>{
     const newUser = {
@@ -17,10 +20,19 @@ app.post('/api/users', (req, res)=>{
     users.push(newUser)
     res.status(201).json({message: 'New user successfully created!', data: users})
 })
-app.delete('api/users/:id',(req, res)=>{
-    let userId = req.params.id
-    console.log(userId)
-    res.status(200).json({message:`${userId}`})
+app.delete('/api/users/:id',(req, res)=>{
+    let userId = parseInt(req.params.id)
+    // Check if user exists
+    let userIndex = users.findIndex(user => user.id === userId);
+    if (userIndex === -1) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    // Remove user from the array
+    users = users.filter(user => user.id !== userId);
+
+    // Send response
+    res.status(200).json({ message: 'User deleted successfully', users });
+
 })
 app.listen(PORT, ()=>{
     console.log(`The server is listening on http://localhost:${PORT}`)
